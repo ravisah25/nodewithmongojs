bee623e33ee5123d48923cd5c5207064edc4bcc7
unknown <rshankar.raja@gmail.com>
2019-01-27 18:49:44 +0530

    gh

--- src/models/employee.js
+++ src/models/employee.js
@@ -2,7 +2,7 @@ var db = require('./index')
 
 async function employeeGet() {
     return new Promise((resolve, reject) => {
-        db.employee.find({}, (err, docs) => {
+        db.employee.find().sort({ name: 1 }, (err, docs) => {
             if (!err) {
                 resolve(docs);
             }
@@ -10,6 +10,14 @@ async function employeeGet() {
     })
 }
 
+async function employeePost(data) {
+    return new Promise((resolve, reject) => {
+        db.employee.insert(data, (err, docs) => {
+            resolve(docs);
+        })
+    })
+}
 
-module.exports = { employeeGet }
+
+module.exports = { employeeGet, employeePost }
 
--- src/router/employee.js
+++ src/router/employee.js
@@ -1,9 +1,13 @@
 const employeesRouter = require('express').Router()
-const { employeeGet } = require('../models/employee')
+const { employeeGet, employeePost } = require('../models/employee')
 
 employeesRouter.get('/', async (req, res) => {
-
     let data = await employeeGet();
+    res.send(data);
+})
+
+employeesRouter.post('/', async (req, res) => {
+    let data = await employeePost(req.body);
     res.send(data);
 })
 
